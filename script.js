efb45f45fc6784bdd14a8167ba0349fd09d25ea4
peryshah <97086541+peryshah@users.noreply.github.com>
2023-08-31 17:54:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,139 +1,27 @@
-// ways to print in Javascript.  
 
-// document.write("Best Industrial Plastic Manufacturer of ")
-// document.write("UHMWPE, HDPE, PP, DELRIN, TEFLON, CAST NYLON")
+gsap.from(".image-container", {
+        ease: Expo.easeInOut,
+        width: "0%",
+        duration: 2,
+        stagger: 1,
+        
+})
 
-
-          // ways to print in console log.
-// console.log("Hello World", 4 + 6, "another log");
-// console.warn("This is warning")
-// console.error("This is an error")
-
-        // java scrpit variables
-// var number1 = 80;
-// var number2 = 20;
-// console.log(number1 + number2);
-
-
-        // Data types in javascript
-     // Numbers 
-// var num1 = 45;
-// var num2 = 36;
-//     // String
-// var str1 = "This is Parin";
-// var str2 = "This is my school";
-//     // Object
-// var marks = {
-//     Parin: 80, Bhargav: 70, Jigo: 60
-// }         
-
-// console.log(marks);
-
-
-        // booleans
-// var a = true;
-// var b = false;
-// console.log(a, b);
-// var arr = [1,2,3,4,5]
-
-//         //Arithmetic Operators
-// var a = 100;
-// var b = 10;
-// // console.log("The value of a+b is", a+b);
-// // console.log("The value of a-b is", a-b);
-// // console.log("The value of a*b is", a*b);
-// // console.log("The value of a/b is", a/b);
-
-//         // Assigment Operators
-// var c = b;
-// c += 2
-// // console.log(c);
-
-//         // camparision operators
-// var x = 60;
-// var y = 50;
-// console.log(x == y);
-// console.log(x <= y);
-// console.log(x >= y);
-// console.log(x < y);
-// console.log(x > y); 
-
-        //if else statment
-/*
-        var age = 16;
-if (age > 18){
-    console.log("You are an adult")
-}
-else{
-    console.log("You are not an adult")
-}
-*/
-        // Loops
-// var arr = [1,2,3,4,5,6,7];
-// console.log(arr);
-// for(var i=0;i<arr.length;i++){
-//     if(i==2){
-//         // break;
-//         continue;
-//     }
-//     console.log(arr[i])
-// }       
-        // while loop
-// let j=0;
-// while(j<arr.length){
-//     console.log(arr[j]);
-//     j++;
-// }
-//     Array Methods
-// let myArr = ["Parin", "Dhwani", 69, null, true];
-// console.log(myArr.length)
-// // myArr.pop();
-// // myArr.push("Jia")
-// // myArr.shift()
-// // myArr.unshift("Aarya")
-
-// console.log(myArr);
-        // String Methods
-// let mystring = "Parin is a good boy"
-// console.log(mystring); {get print your string/ copy your string}
-// console.log(mystring.length); {get print your string length(total character)}
-// console.log(mystring.indexOf("good")); {get print index of the word in the string(good is at 11th index)}
-// console.log(mystring.slice(0,5))  {get your string slice at the given point}
-// console.log(mystring.replace("Parin", "Nihal"));  {get print to replace any word from the string.}
-        // how to get dates in JavaScript.
-// Mydate = new Date();
-// Mydate = new Date();
-// console.log(Mydate.getTime()); {get time in secound}
-// console.log(Mydate.getFullYear()); {fet current year}
-// console.log(Mydate.getDay()); {get day of the week}
-// console.log(Mydate.getMinutes()); {get current minutes running}
-// console.log(Mydate.getHours()); {get current hour running}
-
-        // DOM manuplations
-
-// let elem = document.getElementById('click');
-// console.log(elem)
-
-// let elemclass = document.getElementsByClassName('container');
-// console.log(elemclass)
-// // elemclass[0].style.background = "yellow"
-// // elemclass[1].style.background = "blueviolet"
-
-// elemclass[0].classList.add("bg-primary")
-//         //  functions/events search other events on Internet
-// function clicked(){
-//         console.log('The button was clicked')
-// }
-// window.onload = function(){
-//         console.log('The window was loaded')
-// }
-// firstContainer.addEventListener('click', function(){
-//         console.log("It was clicked")
-// })
-
-
-
-
-    
-
-
+gsap.from(".heading h1",{
+        y: -100,
+        duration:1,
+        delay:1,
+        opacity:0
+})
+gsap.from(".heading img",{
+        x: -100,
+        duration:1,
+        delay:0.5,
+        opacity:0
+})
+gsap.from(".navbar",{
+        y: -100,
+        duration:1,
+        delay:0.5,
+        opacity:0
+})
