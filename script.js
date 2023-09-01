5088613591a4c5e2217e6f53e0b641edd160caa2
peryshah <97086541+peryshah@users.noreply.github.com>
2023-09-01 13:44:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ gsap.from(".image-container", {
         width: "0%",
         duration: 2,
         stagger: 1,
-        
+        opacity: 0
 })
 
-gsap.from(".heading h1",{
+gsap.from(".heading h1", {
         y: -100,
-        duration:1,
-        delay:1,
-        opacity:0
+        duration: 1,
+        delay: 1,
+        opacity: 0
 })
-gsap.from(".heading img",{
+gsap.from(".heading img", {
         x: -100,
-        duration:1,
-        delay:0.5,
-        opacity:0
+        duration: 1,
+        delay: 0.5,
+        opacity: 0
 })
-gsap.from(".navbar",{
+gsap.from(".navbar", {
         y: -100,
-        duration:1,
-        delay:0.5,
-        opacity:0
+        duration: 1,
+        delay: 0.5,
+        opacity: 0
 })
